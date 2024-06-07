a2687fc665aad018eceabaace87b3690811d8c56
Dmitriy Popov <145620090+iLooToS@users.noreply.github.com>
2024-06-07 15:51:36 +0300

    fix/movement

--- rego.js
+++ rego.js
@@ -49,8 +49,8 @@ async function updateRating(name) { // после смерти обновляе�
     const users = await User.findOne({ where: { name }, attributes: ['rating_id', 'name'], raw: true });
     const ratu = await Rating.findAll({ where: { score }, attributes: ['id', 'score'], raw: true });
     const result = ratu.find(el => el.id === users.rating_id)
-    const ratu = result.score; 
-    const result = await Rating.update(
+    const ratu1 = result.score; 
+    const result1 = await Rating.update(
       { score: ratu + 1 },
       { where: { id: users.id } },
     );
